e33ea6d359f94487b9f039157be58b5991be44b5
ido4848 <ido.aizenbud@gmail.com>
2015-10-14 11:52:47 +0300

    error button added

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,76 @@ adj
 kishor=[של,על,ב,ו,ל,ה]
  */
 
+function checkTime_util(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function checkTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime_util(m);
+    s = checkTime_util(s);
+    var time= h + ":" + m + ":" + s;
+    return time;
+}
+
+function checkDate(){
+	var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd;
+    } 
+    if(mm<10){
+        mm='0'+mm;
+    } 
+    var date = dd+'/'+mm+'/'+yyyy;
+    return date;
+}
+
+
+function sendErrorMsg(){
+	var currHaiku1=$("#line1").text();
+	var currHaiku2=$("#line2").text();
+	var currHaiku3=$("#line3").text();
+	var date = checkDate()+", "+checkTime(); 
+
+	var subject="Error Msg in HaikuHeb, "+date;
+	var htmlMsg="This is the problematic haiku: <br>"+currHaiku1+"<br>"+currHaiku2+"<br>"+currHaiku3+"<br>Sent on the: "+date;
+
+	$.ajax({
+	  type: "POST",
+	  url: "https://mandrillapp.com/api/1.0/messages/send.json",
+	  data: {
+	    'key': 'LKnQV8kj9K4Iu1EEqHIqPg',
+	    'message': {
+	      'from_email': 'ido.aizenbud@gmail.com',
+	      'to': [
+	          {
+	            'email': 'ido.aizenbud@gmail.com',
+	            'name': 'me',
+	            'type': 'to'
+	          }
+	        ],
+	      'autotext': 'true',
+	      'subject': subject,
+	      'html': htmlMsg
+	    }
+	  }
+	 }).done(function(response) {
+	   console.log(response); // if you're into that sorta thing
+	 });
+	
+}
+
 function getWordFiles(){
 	$.getJSON("../words/verbs.json", function(json) {
 	//console.log(json); // this will show the info it in firebug console
@@ -148,6 +218,8 @@ function main(){
 	$( "#random-line3" ).mousedown(function() {
   		$( "#random-line3" ).attr("src","../img/random_line3_down.jpg");
 	});	
+
+	$("#error").click(sendErrorMsg);
 
 }
 
